7f7208037dd31ec01555ee7f414e17a7bb53f5ac
core.editor <mbeglaryan9@gmail.com>
2019-09-18 03:52:03 -0700

    fixed version

--- input.py
+++
@@ -1,7 +0,0 @@
-age=input("write your age... ")
-print("hi your age is", int(age))
-user_age_plus_ten=int(age)+10
-print("Your age plus 10 will be =",user_age_plus_ten)
-just_int=input("please enter the int smaller then your age...")
-print("your age minus this int will be=",int(age)-int(just_int))
-input("Good bye")

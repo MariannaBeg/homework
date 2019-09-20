1ba30dda9b388ba6dbb04f535299101ebaa173f2
core.editor <mbeglaryan9@gmail.com>
2019-09-20 05:54:50 -0700

    my second git share

---
+++ logical operators1.py
@@ -0,0 +1,10 @@
+my_nieces_first_word="Mayana"
+print("my niece's first word was Mayana")
+print("Is there letter /y/ in it?", "y" in my_nieces_first_word)
+my_nieces_second_word="Mita"
+print("my niece's second word was Mita")
+print("Is there letter /t/ in it?", "t" in my_nieces_second_word)
+print("Is there letter /a/ in both words?", "M" in (my_nieces_second_word and my_nieces_first_word))
+print("Is Mayana is bigger word then Mita?",my_nieces_first_word>my_nieces_second_word)
+print("Is Mayana is equal to Mita?",my_nieces_first_word==my_nieces_second_word)
+print("Is Mayana is not equal to Mita?",my_nieces_first_word != my_nieces_second_word)
---
+++ logical operators2.py
@@ -0,0 +1,18 @@
+there_is_electricity_in_house="yes"
+there_is_no_electricity_in_house="no"
+print("Is there electricity in the house?",there_is_no_electricity_in_house>there_is_electricity_in_house)
+print("Then take off all fruits from the fridge")
+apples=0
+pears=2
+grapes=3
+
+print("Is there apples in the fridge?",apples!=0)
+print("Then we need to buy apples")
+print("Is there pears in the fridge?",pears!=0)
+print("How much pears we have?", pears)
+print("Not too much")
+print("Is there grapes in the fridge?",grapes!=0,", we have them-",grapes)
+print("then we need to buy some of fruits, which are less then 3")
+print("are apples less then 3?",apples<3, "then we need them")
+print("are pears less then 3?",pears<3, "then we need them")
+print("are grapes less then 3?",grapes<3, "then we don't need them")

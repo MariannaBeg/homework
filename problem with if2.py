3d1767463dcfc1c1a3df31e00019e12a3c4985c9
core.editor <mbeglaryan9@gmail.com>
2019-09-25 10:00:46 -0700

    if elif

---
+++ problem with if2.py
@@ -0,0 +1,8 @@
+print("please input the temperature")
+temp=float(input())
+if temp>20:
+	print("U can wear a t-shirt")
+elif temp<0:
+		print("wear a coat")
+else:
+	print("wear something with long-sleeves")

e0e49c10ff3e5b12ff2843c0432ed5df5283bfbf
core.editor <mbeglaryan9@gmail.com>
2019-09-25 08:38:12 -0700

    how to use if

---
+++ if.py
@@ -0,0 +1,14 @@
+#how to use "if" statement
+a="mame"
+b="bye"
+if a<b=="mame":
+	print("a is less then b")
+
+if b<a=="mame":
+	print("a is greater then b")
+
+if a==b or b<a:
+	print("good job")
+
+if "e" in a and b:
+	print("perfect")

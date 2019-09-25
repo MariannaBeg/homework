cb8819fb0ecd1aeded373869c6800959f5b998f4
core.editor <mbeglaryan9@gmail.com>
2019-09-25 09:01:33 -0700

    nested conditions

--- if.py
+++ if.py
@@ -1,14 +1,28 @@
 #how to use "if" statement
-a="mame"
-b="bye"
-if a<b=="mame":
-	print("a is less then b")
+#a="mame"
+#b="bye"
+#if a<b=="mame":
+#	print("a is less then b")
 
-if b<a=="mame":
-	print("a is greater then b")
+#if b<a=="mame":
+#	print("a is greater then b")
 
-if a==b or b<a:
-	print("good job")
+#if a==b or b<a:
+#	print("good job")
 
-if "e" in a and b:
-	print("perfect")
+#if "e" in a and b:
+#	print("perfect")
+weather="cold"
+a=5
+b=3
+message="hello world"
+len_of_Message=len(message)
+print(len_of_Message)
+if weather =="cold" and a>b and len_of_Message > 3:
+	print("it works")
+	if a!=10:
+		print("bravo")
+		if b=="hello":
+		else:
+			print(a)
+
---
+++ nested conditions.py
@@ -0,0 +1,14 @@
+weather="cold"
+a=5
+b=3
+message="hello world"
+len_of_Message=len(message)
+print(len_of_Message)
+if weather =="cold" and a>b and len_of_Message > 3:
+	print("it works")
+	if a!=10:
+		print("bravo")
+		if b=="hello":
+			print(a)
+
+print("I got it:)))")

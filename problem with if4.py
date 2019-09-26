3dea0c67a14f6be0e8940ba2fe38733585ade817
core.editor <mbeglaryan9@gmail.com>
2019-09-26 03:25:50 -0700

    number divisible on 5 and 11

---
+++ problem with if4.py
@@ -0,0 +1,6 @@
+print("Please insert a number")
+num=float(input())
+if num%5==0 and num%11==0:
+	print("this number is divisible to 5 and 11")
+else:
+	print(" this number is not divisible on 5 and 11")

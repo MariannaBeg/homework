a4f398edbc173c79ced6e5aa4c79d7597c7ed0d3
core.editor <mbeglaryan9@gmail.com>
2019-10-01 13:10:18 -0700

    def

--- fizzbuzz with def.py
+++ fizzbuzz with def.py
@@ -5,6 +5,6 @@ def fizz_buzz(num):
 		return f'"Buzz"'
 	elif num%3==0:
 		return f'"Fizz"'
-	return f'"this num is not divisible on 3 or 5"'
+	return f'{num}'
 num=int(input("please insert number -"))
 print(fizz_buzz(num))	

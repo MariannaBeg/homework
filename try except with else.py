5bcce5aa8dea47144af7af739017d063ad0ca44f
core.editor <mbeglaryan9@gmail.com>
2019-10-04 09:40:25 -0700

    errors

---
+++ try except with else.py
@@ -0,0 +1,17 @@
+my_string="12"
+
+try:
+	print ("converting my str to int...")
+	print("string #"+"1"+":"+my_string)
+	my_int=int(my_string)
+	print(my_int)
+except ValueError:
+	print("Can't convert; my_string to a number")
+except TypeError:
+	print("Can't concatinate number with string")
+except:
+	print("Unknown error")
+else:
+	print("No errors occured")
+
+print("Done!")

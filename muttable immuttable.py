7cd0d07a8aaa43a39f73eb1d53e1f6e0a941883f
core.editor <mbeglaryan9@gmail.com>
2019-10-07 10:02:45 -0700

    how to change string characters

---
+++ muttable immuttable.py
@@ -0,0 +1,19 @@
+my_int=14
+
+print(id(my_int))
+
+a="a"
+print (id(a))
+b="a"
+print (id(b))
+# ays depqum erkusi id n el nuyny klini qani vor arjeqy nuynna
+print(a is b)
+list1=[1,2,3]
+print(id(list1))
+list1.append(4)# avelacnum enq listin arjeq
+print(id(list1))
+#ktpi nuyn ID n nshanakum e vor listy mutabble e
+a=[1.2,2,3]
+b=[1,2,3]
+print(a)
+#ktpi tarber ID hascener,da arvum e nra hamar vor ete listum popoxutyun arvi na ir hascen chi poxi, bayc myus listumel chgri nuyn popoxutyuny
---
+++ string styles.py
@@ -0,0 +1,8 @@
+my_string="this is my test string"
+
+print (my_string)
+print(my_string.capitalize())# miayn arajin tary naxadasutyan  mecatar e sarqum
+print(my_string.lower()) # bolory poqratar
+print(my_string.upper()) #bolory barrery mecatar
+print(my_string.title())#bolory barreri arajin tarery mecatar
+print(my_string.replace("my","your"))# bolory my barery stringum your -iva poxarinum

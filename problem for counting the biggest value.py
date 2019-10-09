a2dd1e62661166f6682ece1bf22c2dbf284be732
core.editor <mbeglaryan9@gmail.com>
2019-10-09 09:47:40 -0700

    max

---
+++ problem for counting the biggest value.py
@@ -0,0 +1,8 @@
+def find_biggest(lists):
+	for num_list in lists:
+		biggest_value=0
+		if num_list>biggest_value:
+			biggest_value=num_list
+	return biggest_value
+lists=[2,4,5,6,7,8,10]
+print("The biggest value is:",find_biggest(lists))

50ec584324e46aa0a5427ac657ef710d8b56d62f
core.editor <mbeglaryan9@gmail.com>
2019-10-10 12:55:36 -0700

    lists

---
+++ count elements in list.py
@@ -0,0 +1,7 @@
+input_string = input("Enter a list of elements separated by space ")
+a= input_string.split()
+sum = 0
+for i in a:
+	sum+=1
+print("You entered  ",sum,"elements in the list")    
+    

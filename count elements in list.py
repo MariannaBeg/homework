bed03d294329372989fc223f49928a016fdb0135
MariannaBeg <55292822+MariannaBeg@users.noreply.github.com>
2019-10-10 13:01:08 -0700

    Delete count elements in list.py

--- count elements in list.py
+++
@@ -1,7 +0,0 @@
-input_string = input("Enter a list of elements separated by space ")
-a= input_string.split()
-sum = 0
-for i in a:
-	sum+=1
-print("You entered  ",sum,"elements in the list")    
-    

016003b3aaf8cc3c89f72c2dfd75ddf388b74905
core.editor <mbeglaryan9@gmail.com>
2019-10-14 10:37:36 -0700

    dict

---
+++ how to find words amount in text by dict.py
@@ -0,0 +1,21 @@
+#ete mi textum uzumenq barery gtnenq , hashvenq voric qanisna
+sample_text=" is, simply dummy text .of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to"
+words_dict={}
+
+sample_text=sample_text.lower()# bolory poqratar e sarqum vor heto nuyn barery mecatari patcharov tarber chhamari
+sample_text=sample_text.replace(",","")# sa nra hamar e vor storaketnery jnji
+sample_text=sample_text.replace(".","")# sa nra hamar e vor ketery jnji
+sample_text=sample_text.split(" ")#taranjatum enq bolor barery vor hesht karda
+
+
+# vor gtni chkrknvox barery u avelacni nor words_dictin
+for word in sample_text:
+	if word in words_dict.keys():
+		words_dict[word]+=1
+	else:
+		words_dict[word]=1
+# sa el kazmum enq nra hamar vor tpi miayn 2ic avel krknvox barery
+
+for (word,amount) in words_dict.items():
+	if amount>1:
+		print(word,":",amount)
